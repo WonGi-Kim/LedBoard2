c2392f50b88922cb45e5a66ce9e3a151ea8d25a4
WonGi-Kim <kwg970310@gmail.com>
2024-03-19 22:52:51 +0900

    Initial Commit

---
+++ LedBoard2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LedBoard2
+//
+//  Created by 김원기 on 3/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LedBoard2/LedBoard2App.swift
@@ -0,0 +1,17 @@
+//
+//  LedBoard2App.swift
+//  LedBoard2
+//
+//  Created by 김원기 on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LedBoard2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
